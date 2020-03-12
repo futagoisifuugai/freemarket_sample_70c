adb1a4aac1354a0e0aeb494de772562150bbca62
kanekoshinya <knksny777@icloud.com>
2020-03-12 12:30:29 +0900

    ユーザーエラーメッセージ日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
-  validates :password,presence:true,format: {with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,100}+\z/i,message:"無効なパスワードです"}
-  validates :first_name,presence:true,format: {with:/\A[ぁ-んァ-ン一-龥]/,message:"無効な苗字です"}
-  validates :last_name,presence:true,format: {with:/\A[ぁ-んァ-ン一-龥]/,message:"無効な名前です"}
-  validates :first_name_kana,presence:true,format: {with:/\A[ァ-ヶー－]+\z/,message:"無効な苗字です"}
-  validates :last_name_kana,presence:true,format: {with:/\A[ァ-ヶー－]+\z/,message:"無効な名前です"}
-  validates :email,presence:true,uniqueness: true,format: {with:/\A\S+@\S+\.\S+\z/,message:"無効なメールです"}
+  validates :password,presence:true,format: {with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{7,100}+\z/i}
+  validates :first_name,presence:true,format: {with:/\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name,presence:true,format: {with:/\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana,presence:true,format: {with:/\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana,presence:true,format: {with:/\A[ァ-ヶー－]+\z/}
+  validates :email,presence:true,uniqueness: true,format: {with:/\A\S+@\S+\.\S+\z/}
 end
