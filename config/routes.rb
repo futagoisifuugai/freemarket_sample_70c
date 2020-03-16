5644dbf1e695d8e7180435237e16f6edc7208fa8
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-03-17 06:06:11 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
-  root  "tops#index"
+  root  "products#index"
   resources :tops, only: [:index,:show,:new]  
     
   resources :user_adresses, only: [:index,:new,:create]
