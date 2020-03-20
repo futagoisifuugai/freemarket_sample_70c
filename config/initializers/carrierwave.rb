66574bf3339d84129b6b956acaf86a86fbb1fa05
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-03-20 11:33:22 +0900

    CarrierWave修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,19 +3,24 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-    config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
-    config.storage = :fog
+  # if Rails.env.production?
+  # これしか書いていない状態でstaging環境でテストし、「ArgumentError ( is not a recognized provider):」
+  if Rails.env.production? || Rails.env.staging?
+    config.storage :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'xxxxxxxxxxxxxxxx'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/xxxxxxxxxxxxxxxx'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-# credentials下にaws_access_key_idとaws_secret_access_keyはあるよ
-      region: 'ap-northeast-1'
+      aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+      region: "s3-ap-northeast-1",
+      path_style: true
     }
-    config.fog_directory  = 'freemarketsample70c'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample70c'
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
+
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
